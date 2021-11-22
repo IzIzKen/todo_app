81c69e1fbbca3c05125ba1bb5b8910b4a50e2138
IzIzKen <111420knt@gmail.com>
2021-11-22 14:48:25 +0900

    change layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '_', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSRF Token-->
+    <meta name="csrf-token" content="{{ csrf-token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css')  }}" rel="stylesheet">
+
+</head>
+<body>
+    <div id="app">
+        @component('components.header')
+        @endcomponent
+
+        @component('components.flash')
+        @endcomponent
+
+        <main>
+            <div class="container-fruid h-100 p-2 d-flex flex-column">
+                @yield('content')
+            </div>
+        </main>
+
+        @component('components.footer')
+        @endcomponent
+    </div>
+</body>
+</html>
