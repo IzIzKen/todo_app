dc1888d7670209851689d69e7cae8b5089df348b
IzIzKen <111420knt@gmail.com>
2021-11-25 21:20:28 +0900

    add Tag Todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Todo;
 use App\Models\Goal;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,7 +17,7 @@ class TodoController extends Controller
      */
     public function index(Request $request, Goal $goal)
     {
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
         return  response()->json($todos);
     }
 
@@ -36,7 +37,7 @@ class TodoController extends Controller
         $todo->position = request('position');
         $todo->done = false;
         $todo->save();
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
         return response()->json($todos);
     }
 
@@ -57,7 +58,7 @@ class TodoController extends Controller
         $todo->done = (bool) request('done');
         $todo->save();
 
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
         return response()->json($todos);
     }
 
@@ -72,11 +73,13 @@ class TodoController extends Controller
     public function destroy(Request $request, Goal $goal, Todo $todo)
     {
         $todo->delete();
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
         return response()->json($todos);
     }
 
-
+    /*
+     *
+     */
     public function sort(Request $request, Goal $goal, Todo $todo)
     {
         $exchangeTodo = Todo::where('position', request('sortId'))->first();
@@ -92,8 +95,32 @@ class TodoController extends Controller
             $todo->moveAfter($exchangeTodo);
         }
 
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
         return response()->json($todos);
 
+    }
+
+    /*
+     *
+     */
+    public function addTag(Request $request, Goal $goal, Todo $todo, Tag $tag)
+    {
+        $todo->tags()->attach($tag->id);
+
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+
+        return response()->json($todos);
+    }
+
+    /*
+     *
+     */
+    public function removeTag(Request $request, Goal $goal, Todo $todo, Tag $tag)
+    {
+        $todo->tags()->detach($tag->id);
+
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+
+        return response()->json($todos);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,9 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
